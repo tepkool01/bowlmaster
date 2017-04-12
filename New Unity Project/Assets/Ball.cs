2fd670b8c69e633e858322a28b24db4b9c7a765d
Michael Young <mvy1221@gmail.com>
2017-04-12 12:24:58 -0700

    '191'

---
+++ New Unity Project/Assets/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float launchSpeed;
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = this.GetComponent<Rigidbody>();
+        audioSource = this.GetComponent<AudioSource>();
+
+        Launch();
+    }
+
+    public void Launch()
+    {
+        rigidBody.velocity = new Vector3(0, 0, launchSpeed);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+}
